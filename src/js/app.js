c7195095ef1ca9827e8d18320a0233a27d2b8ef3
waanh <anhimovalera@gmail.com>
2024-09-08 21:07:20 +0300

    done

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
 console.log('app worked');
+
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js';
+
+const game = new Game();
+game.start();
+
+loadGame();
+saveGame();
